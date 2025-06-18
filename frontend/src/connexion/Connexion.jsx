d59d5aaea77ab685e2980d9266624a182fbf9e22
Lucien Lafaye <lafaye@et.intechinfo.fr>
2025-06-18 22:42:10 +0200

    Fix url backend api requests

--- frontend/src/connexion/Connexion.jsx
+++ frontend/src/connexion/Connexion.jsx
@@ -16,7 +16,7 @@ function Connexion() {
     }
 
     try {
-      const response = await fetch('http://localhost:3001/api/auth/login', {
+      const response = await fetch('http://localhost:8080/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
--- frontend/src/inscription/Inscription.jsx
+++ frontend/src/inscription/Inscription.jsx
@@ -15,7 +15,7 @@ function Inscription() {
     }
 
     try {
-      const response = await fetch('http://localhost:3001/api/auth/register', {
+      const response = await fetch('http://localhost:8080/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
